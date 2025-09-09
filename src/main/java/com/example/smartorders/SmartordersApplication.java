1744f94d72d640bd5370f8de80e3e4163ef8047e
Anjo <chuables21@gmail.com>
2025-09-09 09:55:36 +0800

    Starter Spring project

---
+++ src/main/java/com/example/smartorders/SmartordersApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartorders/SmartordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
